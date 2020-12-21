f074e715673a8369f44325219ad196bc25f4fdb3
Manjot Singh Sindhu <31506001+ManjotSinghSindhu@users.noreply.github.com>
2020-12-21 15:02:21 +0530

    Add files via upload

---
+++ ARITHMETIC OPN.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 08:49:26 2018
+
+@author: Manjot singh
+"""
+
+a,b=12,6
+print("add : ",a+b)
+print("sub : ",a-b)
+print("mul : ",a*b)
+print("div : ",a/b)
+print("mod : ",a%b)
+print("pow : ",a**b)
+print("floor : ",a//b)
