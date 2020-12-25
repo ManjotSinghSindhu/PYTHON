228178b4902e21a6ccc8300ebe567b77708aff3f
Manjot Singh Sindhu <31506001+ManjotSinghSindhu@users.noreply.github.com>
2020-12-25 20:40:09 +0530

    Add files via upload

---
+++ ASSIGNMENT OPN.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 08:51:57 2018
+
+@author: Manjot singh
+"""
+
+a=b=c=10
+print(a,b,c)
+
+x,y=10,5
+x+=y
+print(x)
+x*y
+print(x)
---
+++ COMPARISION OPN.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 18 08:53:13 2018
+
+@author: Manjot singh
+"""
+
+x,y=77,88
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+print(x>>y)
